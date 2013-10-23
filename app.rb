bac8e113d8ec3af7384f54fb4bf4050d7d2f08ad
bbeaird <bbeaird@gmail.com>
2013-10-23 14:44:02 -0700

    yay survay

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get '/' do
 end
 
 post '/welcome' do
-  p session[:gsnake_id] = params[:gsnake_id]
+  session[:gsnake_id] = params[:gsnake_id]
   redirect '/survey'
 end
 
@@ -22,10 +22,9 @@ get '/survey' do
 end
 
 post '/rate_movie' do
-  p params
   params.keys.each_with_index do |item,index|
     if item.to_i >=0
-      MovieRating.create!(gsnake_id: session[:gsnake_id], movie_id: params.keys[index].to_i, rating: params.values[index])
+      MovieRating.create!(gsnake_id: session[:gsnake_id], movie_id: params.keys[index].to_i, rating: params.values[index].to_i)
     end
   end
   redirect '/thanks'
@@ -34,42 +33,3 @@ end
 get '/thanks' do
   erb :thanks
 end
-
-
-# post '/sign_in_verify' do
-#   login(params[:name])
-
-#   if signed_in_user
-#     redirect '/user_home'
-#   else
-#     redirect '/'
-#   end
-# end
-
-# get '/user_home' do
-#   erb :user_home
-# end
-
-# post '/sign_out' do
-#   session.delete :name
-#   redirect '/'
-# end
-
-
-# helpers do
-#   def login(name)
-#     user = User.find_by_name(name)
-
-#     unless user.nil?
-#       session[:name] = user.id
-#     end
-#   end
-
-#   def signed_in_user
-#     current_user
-#   end
-
-#   def current_user
-#     @current_user ||= User.find_by_id(session[:name]) if session[:name]
-#   end
-# end
