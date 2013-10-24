3e0d70f0525bab5301025e9966bfdb7b0b13b362
bbeaird <bbeaird@gmail.com>
2013-10-23 22:15:19 -0700

    refactoring filez

--- app.rb
+++ app.rb
@@ -9,40 +9,6 @@ enable :sessions
 
 set :database, "postgres://localhost/gophlix"
 
-get '/' do
-  erb :welcome
-end
-
-post '/welcome' do
-  session[:gsnake_id] = params[:gsnake_id]
-
-  @x = current_user.run_matches
-  content_type :json
-  
-  return @x.to_json
-end
-
-get '/my_movie_map' do 
-  erb :my_movie_map
-end
-
-get '/survey' do
-  erb :survey
-end
-
-post '/rate_movie' do
-  params.keys.each_with_index do |item,index|
-    if item.to_i >=0
-      MovieRating.create!(gsnake_id: session[:gsnake_id], movie_id: params.keys[index].to_i, rating: params.values[index].to_i)
-    end
-  end
-  redirect '/thanks'
-end
-
-get '/thanks' do
-  erb :thanks
-end
-
 helpers do
   def signed_in_user
     current_user
@@ -51,4 +17,37 @@ helpers do
   def current_user
     @current_user ||= Gsnake.find_by_id(session[:gsnake_id]) if session[:gsnake_id]
   end
+end
+
+get '/' do
+  erb :welcome
+end
+
+post '/welcome' do
+  session[:gsnake_id] = params[:gsnake_id]
+
+  data = current_user.run_matches
+
+  content_type :json
+  return data.to_json
+end
+
+get '/survey' do
+  erb :survey
+end
+
+post '/rate_movie' do
+  this_movie_rating = MovieRating.find_by(gsnake_id: session[:gsnake_id])
+  Movie.all.each_with_index do |movie,index|
+    if this_movie_rating
+      MovieRating.update(this_movie_rating.id, gsnake_id: session[:gsnake_id], movie_id: params.keys[index].to_i, rating: params.values[index])
+    else
+      MovieRating.create(gsnake_id: session[:gsnake_id], movie_id: params.keys[index].to_i, rating: params.values[index])
+    end
+  end
+  redirect '/thanks'
+end
+
+get '/thanks' do
+  erb :thanks
 end
